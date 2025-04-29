889d6878f1e4d840cbab274f43b986befb4e27a0
dangered wolf <d@ngeredwolf.me>
2025-04-29 13:01:59 -0700

    Revert "chore: update user query"
    This reverts commit c9aa104349ba4fd71062bf387143446d62830720.

--- src/providers/twitter/graphql/queries.ts
+++ src/providers/twitter/graphql/queries.ts
@@ -158,7 +158,7 @@ export const TweetResultsByIdsQuery: GraphQLQuery = {
   requiresAccount: true,
   variables: {
     includeTweetImpression: true,
-    includeHasBirdwatchNotes: true,
+    includeHasBirdwatchNotes: false,
     includeEditPerspective: false,
     rest_ids: [],
     includeEditControl: true,
@@ -196,7 +196,7 @@ export const TweetResultByIdQuery: GraphQLQuery = {
   requiresAccount: true,
   variables: {
     includeTweetImpression: true,
-    includeHasBirdwatchNotes: true,
+    includeHasBirdwatchNotes: false,
     includeEditPerspective: false,
     includeEditControl: true,
     includeCommunityTweetRelationship: true,
@@ -229,27 +229,18 @@ export const TweetResultByIdQuery: GraphQLQuery = {
 
 export const UserByScreenNameQuery: GraphQLQuery = {
   httpMethod: 'GET',
-  queryId: '1VOOyvKkiI3FMmkeDNxM9A',
+  queryId: 'sLVLhk0bGj3MVFEKTdax1w',
   queryName: 'UserByScreenName',
   requiresAccount: false,
   variables: {
-    screen_name: ''
+    screen_name: '',
+    withSafetyModeUserFields: true
   },
   features: {
-    hidden_profile_subscriptions_enabled: true,
-    profile_label_improvements_pcf_label_in_post_enabled: true,
-    rweb_tipjar_consumption_enabled: true,
-    verified_phone_label_enabled: false,
-    subscriptions_verification_info_is_identity_verified_enabled: true,
-    subscriptions_verification_info_verified_since_enabled: true,
-    highlights_tweets_tab_ui_enabled: true,
-    responsive_web_twitter_article_notes_tab_enabled: true,
-    subscriptions_feature_can_gift_premium: true,
-    creator_subscriptions_tweet_preview_api_enabled: true,
+    blue_business_profile_image_shape_enabled: true,
+    responsive_web_graphql_exclude_directive_enabled: true,
     responsive_web_graphql_skip_user_profile_image_extensions_enabled: false,
-    responsive_web_graphql_timeline_navigation_enabled: true
-  },
-  fieldToggles: {
-    withAuxiliaryUserLabels: true
+    responsive_web_graphql_timeline_navigation_enabled: false,
+    verified_phone_label_enabled: true
   }
 };

cf7b042f557c8befd909bbd8fad748bf82514b8e
dangered wolf <d@ngeredwolf.me>
2025-12-30 20:02:03 -0800

    chore: fix comments

--- src/realms/tiktok/routes/activity.ts
+++ src/realms/tiktok/routes/activity.ts
@@ -7,7 +7,7 @@ import { handleActivity } from '../../../embed/activity';
 export const activityRequest = async (c: Context) => {
   const { snowcode } = c.req.param();
 
-  /* This throws the necessary data to handleStatus (in status.ts) */
+  /* This throws the necessary data to handleActivity (in activity.ts) */
   const statusResponse = await handleActivity(c, snowcode ?? '0', DataProvider.TikTok);
 
   if (statusResponse) {

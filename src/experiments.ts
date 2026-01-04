12d8165c79bc67be112042fd907e7d86572aa4ae
dangered wolf <d@ngeredwolf.me>
2026-01-03 23:21:31 -0800

    chore: re-enable video redirect workaround for twitter/bsky

--- src/experiments.ts
+++ src/experiments.ts
@@ -45,7 +45,7 @@ const Experiments: { [key in Experiment]: ExperimentConfig } = {
   [Experiment.VIDEO_REDIRECT_WORKAROUND]: {
     name: 'Video redirect workaround',
     description: 'Workaround for video playback issues on Telegram/Discord',
-    percentage: 0
+    percentage: 1
   },
   [Experiment.ACTIVITY_EMBED]: {
     name: 'Discord activity embed',
